c93f01df36c592ed940a9a6881e4c97be96ab4a6
Choki <oleszczakhanna@gmail.com>
2024-06-25 19:56:27 +0200

    added way to add new wpis

--- src/dzien2_frontend/src/components/Blog.vue
+++ src/dzien2_frontend/src/components/Blog.vue
@@ -3,18 +3,24 @@
         <button @click="pobierzWpisy">refresh</button>
         blog.
         {{ wpisy }}
+        <input v-model="nowyBlog" type="text">
+        <button @click="dodajWpisy">dodaj</button>
     </div>
 </template>
 
 <script>
-import { dzien2_backend } from 'declarations/dzien2_backend/index'
+import { dzien2_backend } from 'declarations/dzien2_backend/index';
 export default {
     data() {
         return {
-            wpisy: []
+            wpisy: [],
+            nowyBlog: ""
         }
     },
     methods: {
+        async dodajWpisy() {
+            await dzien2_backend.dodaj_wpisy(this.nowyBlog);
+        },
         async pobierzWpisy() {
             this.wpisy = await dzien2_backend.odczytaj_wpisy();
         }
